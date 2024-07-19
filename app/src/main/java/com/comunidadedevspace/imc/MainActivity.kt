e48be3520d03fdf6023ea04c4a6bb59919f43502
Thaila Vieira <thailavs.adv@gmail.com>
2024-07-19 18:52:42 +0100

    Tela inicial do aplicativo

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,29 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar os componentes EditText
+            //Criar uma variável e associar o componente de UI <EditText>
+        // Colocar ação no botão setOnClickListener
+
+        val peso = findViewById<TextInputEditText>(R.id.pesoEditText)
+        val altura = findViewById<TextInputEditText>(R.id.alturaEditText)
+        val calcular = findViewById<Button>(R.id.cal_button)
+
+        calcular.setOnClickListener {
+            val peso = peso.text
+            val altura = altura.text
+            println("meu primeiro aplicativo teste" + peso)
+        }
+
+
+
     }
 }
